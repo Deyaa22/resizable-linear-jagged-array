5b778c872dbc33b0c43eff788c7df72aa27941cb
Deyaa Omer <61560614+Deyaa22@users.noreply.github.com>
2024-08-31 17:24:11 +0300

    Add detailed XML documentation to ResizableLinearJaggedArray class

--- ResizableLinearJaggedArray.cs
+++ ResizableLinearJaggedArray.cs
@@ -1,5 +1,28 @@
 ﻿namespace ResizableLinearJaggedArray.Generics;
 
+/// <summary>
+/// RLJArray: A generic resizable linear array with a jagged array as internal structure,
+/// Provides Big-Oh (1) of Time Complexity for accessing items.
+/// 
+/// The array is divided into segments,
+/// Each segment represents a fixed number of contiguous items,
+/// This design saves memory by avoiding the allocation of space for empty segments.
+/// 
+/// RLJArray gives the ability to enlarge and shrink the array as needed.
+/// 
+/// Segment is conceptual term which expresses a contiguous elements in memory with fixed size similar to a standard array.
+/// 
+/// Technically, a segment is a sub array with a fixed length,
+/// but it remains null until at least one element is assigned.
+/// 
+/// The nullability of segments does not affect data retrieval,
+/// Accessing elements returns the default value; if the segment is null,
+/// This approach conserves memory by avoiding the allocation of space of sub array for empty segments.
+/// 
+/// User can get benefit of manually freeing space of memory of segments which get empty by calling the [CleanEmptySegments()] method.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+
 public class ResizableLinearJaggedArray<T>
 {
     private T[][] array;
