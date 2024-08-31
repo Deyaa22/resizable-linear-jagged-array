facd61bad32f448c462f24455699af516b5ef8b0
Deyaa Omer <61560614+Deyaa22@users.noreply.github.com>
2024-08-31 16:19:20 +0300

    Apply custom formatting to ResizableLinearJaggedArray.cs

--- ResizableLinearJaggedArray.cs
+++ ResizableLinearJaggedArray.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResizableLinearJaggedArray
+namespace ResizableLinearJaggedArray;
+
+internal class ResizableLinearJaggedArray
 {
-    internal class ResizableLinearJaggedArray
-    {
-    }
+
 }
